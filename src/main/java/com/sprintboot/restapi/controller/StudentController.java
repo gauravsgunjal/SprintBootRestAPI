7152fa660e83b432e3cd264d9819aa25bc481298
Gaurav Gunjal <gauravsgunjal@gmail.com>
2026-06-13 23:55:51 +0530

    API response updated

--- src/main/java/com/sprintboot/restapi/controller/StudentController.java
+++ src/main/java/com/sprintboot/restapi/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.sprintboot.restapi.controller;
 
 import com.sprintboot.restapi.entity.Student;
+import com.sprintboot.restapi.repository.ApiResponse;
 import com.sprintboot.restapi.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,36 +17,71 @@ public class StudentController {
 
     @Autowired
     StudentRepository studentRepository;
-    //Get All the Students
+
     @GetMapping("/students")
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
+    public ResponseEntity<ApiResponse<List<Student>>> getAllStudents() {
+        List<Student> students = studentRepository.findAll();
+        ApiResponse<List<Student>> response =new ApiResponse<>(
+            HttpStatus.OK.value(),
+            "Students fetched successfully",
+            students
+        );
+
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/students/{id}")
-    public Student getStudentById(@PathVariable Integer id) {
-        return studentRepository.findById(id).get();
+    public ResponseEntity<ApiResponse<Student>> getStudentById(@PathVariable Integer id) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student not found"));
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "Student fetched successfully",
+                student
+            )
+        );
     }
 
     @PostMapping("/student/add")
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public Student addStudent(@RequestBody Student student) {
-        return studentRepository.save(student);
+    public ResponseEntity<ApiResponse<Student>> addStudent(@RequestBody Student student) {
+        Student savedStudent = studentRepository.save(student);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+        new ApiResponse<>(
+            HttpStatus.CREATED.value(),
+            "Student added successfully",
+            savedStudent
+            )
+        );
     }
 
     @PostMapping("/student/update")
-    @ResponseStatus(HttpStatus.OK)
-    public Student updateStudent(@RequestBody Student student) {
+    public ResponseEntity<ApiResponse<Student>> updateStudent(@RequestBody Student student) {
         Student existingStudent = studentRepository.findById(student.getRollNo()).orElseThrow(() -> new RuntimeException("Student not found"));
+
         existingStudent.setFullName(student.getFullName());
         existingStudent.setPercentage(student.getPercentage());
         existingStudent.setBranch(student.getBranch());
-        return studentRepository.save(existingStudent);
+        Student updatedStudent = studentRepository.save(existingStudent);
+
+        return ResponseEntity.ok(new ApiResponse<>(
+            HttpStatus.OK.value(),
+            "Student updated successfully",
+            updatedStudent)
+        );
     }
 
     @DeleteMapping("/student/delete/{id}")
-    public void deleteStudent(@PathVariable Integer id) {
-        Student student = studentRepository.findById(id).get();
+
+    public ResponseEntity<ApiResponse<Object>> deleteStudent(@PathVariable Integer id) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student not found"));
         studentRepository.delete(student);
+
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "Student deleted successfully",
+                null
+            )
+        );
     }
 }
---
+++ src/main/java/com/sprintboot/restapi/repository/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.sprintboot.restapi.repository;
+
+public class ApiResponse<T> {
+
+    private final Integer status;
+    private final String message;
+    private final T data;
+
+    public ApiResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
